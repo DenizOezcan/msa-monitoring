2a6626ab58d7eaab88d6088d84293d61688d8d0b
Deniz Özcan <deniz.oezcan@finware.solutions>
2024-03-08 08:28:59 +0100

    renamed artifact, renamed packages

--- src/main/java/msa/project/monitoring/backend/Application.java
+++ src/main/java/msa/project/monitoring/backend/Application.java
@@ -1,13 +1,13 @@
-package at.msa.project.one;
+package msa.project.monitoring.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OneApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OneApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/msa/project/monitoring/backend/ApplicationTests.java
+++ src/test/java/msa/project/monitoring/backend/ApplicationTests.java
@@ -1,10 +1,10 @@
-package at.msa.project.one;
+package msa.project.monitoring.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OneApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
